623efa2fe49ac5118e7c98ec79d3f48deccf45e3
Soham Padia <sohampadia10@gmail.com>
2023-03-11 19:11:14 +0530

    ok

--- Web/src/components/PopularCategories.jsx
+++ Web/src/components/PopularCategories.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ImageDisplayer } from './ImageDisplayer'
 
 export const PopularCategories = () => {
   return (
@@ -11,23 +12,15 @@ export const PopularCategories = () => {
 
         {/* photos */}
         <div className='h-4/5 w-full grid grid-cols-4 gap-3 px-48 content-evenly justify-items-center'>
-            {newFunction()}
-            <img src="img/shoes.png" alt="" />
-            <img src="img/clothing.png" alt="" />
-            <img src="img/eyewear.png" alt="" />
-            <img src="img/fragrances.png" alt="" />
-            <img src="img/handbags.png" alt="" />
-            <img src="img/home_decor.png" alt="" />
-            <img src="img/jewellery.png" alt="" />
+            <ImageDisplayer pn="shoes.png" pnn="Shoes"></ImageDisplayer>
+            <ImageDisplayer pn="clothing.png" pnn="Clothing"></ImageDisplayer>
+            <ImageDisplayer pn="eyewear.png" pnn="Shoes"></ImageDisplayer>
+            <ImageDisplayer pn="shoes.png" pnn="Shoes"></ImageDisplayer>
+            <ImageDisplayer pn="shoes.png" pnn="Shoes"></ImageDisplayer>
+            <ImageDisplayer pn="shoes.png" pnn="Shoes"></ImageDisplayer>
+            <ImageDisplayer pn="shoes.png" pnn="Shoes"></ImageDisplayer>
+            <ImageDisplayer pn="shoes.png" pnn="Shoes"></ImageDisplayer>
         </div>
     </div>
   )
 }
-function newFunction(pngName) {
-    return <div className="bg-[url(`img/${pngName}.png`)] w-[235px] flex items-end pb-2 pl-1 justify-center">
-        <div className='pb-2 flex justify-center text-white bg-[#00000055] w-full rounded-b-xl'>
-            <p className=''>Shoes</p>
-        </div>
-    </div>
-}
-
