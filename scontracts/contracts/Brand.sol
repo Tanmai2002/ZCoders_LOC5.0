799896f0e7526e96c9bed08952e594977d28af77
mvs1903 <101926677+mvs1903@users.noreply.github.com>
2023-03-12 09:46:48 +0530

    brand updated

--- scontracts/contracts/Brand.sol
+++ scontracts/contracts/Brand.sol
@@ -1,28 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-import "./ServerContract.sol";
-import "./Tracking.sol";
+
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract ServerContract {
+  constructor() {
+  }
+
+//   function createBrand (string memory name,string memory symbol)external  returns (Brand){
+//     Brand b= new Brand(name,symbol);
+//     return b;
+//   }
+  
+}
+
+
 contract Brand is ERC721Enumerable, Ownable {
     using Strings for uint256;
     
 
 constructor(
-  string memory _name,
-  string memory _symbol
-) ERC721(_name, _symbol) {
-  
+    string memory _name,
+    string memory _brandsymbol
+) ERC721(_name, _brandsymbol) {
 }
 
     mapping(string => uint8) existingURIs;
     mapping(uint256 => address) public holderOf;
     uint256 public supply = 0; //determines the id
     uint256 public totalTxn = 0;
+    uint256 public pdt =0;
     uint256 public cost = 0.0001 ether;
 
     event Sale(
@@ -37,23 +48,14 @@ constructor(
 
     TransactionStruct[] transactions;
     TransactionStruct[] minted;
-    ProductStruct [] Product;
-    struct ProductStruct{
-      uint256 prodId;
-      string BrandName;
-      string title;
-      string description;
-      uint256 currentCost;
-      address prod_Owner;
-
-    }
-
+    
     struct TransactionStruct {
         uint256 id;
+        uint256 prod_Id;
         address owner;
-        uint256 cost;
         string title;
         string description;
+        uint256 cost;
         string metadataURI;
         uint256 timestamp;
     }
@@ -65,14 +67,16 @@ constructor(
         require(msg.sender != owner(), "Sales not allowed!");
 
         supply++;
+        pdt++;
 
         minted.push(
             TransactionStruct(
                 supply,
+                pdt,
                 msg.sender,
-                salesPrice,
                 title,
                 description,
+                salesPrice,
                 metadataURI,
                 block.timestamp
             )
@@ -91,59 +95,7 @@ constructor(
         holderOf[supply] = msg.sender;
 
     }
-    // function sellProduct(){
-
-
-    // }
-
-    // function transferProduct(){
-
-    // }
-
-    // function changePrice(){
-
-    // }
-    // function payToMint(
-    //     string memory title,
-    //     string memory description,
-    //     string memory metadataURI,
-    //     uint256 salesPrice
-    // ) external payable {
-
-    //     require(msg.value >= cost, msg.value.toString());
-    //     require(existingURIs[metadataURI] == 0, "This NFT is already minted!");
-    //     require(msg.sender != owner(), "Sales not allowed!");
-        
-
-        
-
-        // supply++;
-
-        // minted.push(
-        //     TransactionStruct(
-        //         supply,
-        //         msg.sender,
-        //         salesPrice,
-        //         title,
-        //         description,
-        //         metadataURI,
-        //         block.timestamp
-        //     )
-        // );
-
-        // emit Sale(
-        //     supply,
-        //     msg.sender,
-        //     msg.value,
-        //     metadataURI,
-        //     block.timestamp
-        // );
-
-        // _safeMint(msg.sender, supply);
-        // existingURIs[metadataURI] = 1;
-        // holderOf[supply] = msg.sender;
-    // }
-
+    
     function sellProduct(uint256 id) external payable {
         require(msg.value >= minted[id - 1].cost, "Ether too low for purchase!");
         require(msg.sender != minted[id - 1].owner, "Operation Not Allowed!");
@@ -153,15 +105,17 @@ constructor(
         transactions.push(
             TransactionStruct(
                 totalTxn,
+                minted[id - 1].prod_Id,
                 msg.sender,
-                msg.value,
                 minted[id - 1].title,
                 minted[id - 1].description,
+                msg.value,
                 minted[id - 1].metadataURI,
                 block.timestamp
             )
         );
-
+        
+        
         emit Sale(
             totalTxn,
             msg.sender,
@@ -173,13 +127,19 @@ constructor(
         minted[id - 1].owner = msg.sender;
     }
 
+    function TransferProduct(uint256 _id,address _new) external{
+        require(msg.sender == minted[_id - 1].owner, "You do not have right to transfer");
+        minted[_id - 1].owner=_new;
+    }
+
     function changePrice(uint256 id, uint256 newPrice) external returns (bool) {
         require(newPrice > 0 ether, "Ether too low!");
-        require(msg.sender == minted[id - 1].owner, "Operation Not Allowed!");
-
+        require(msg.sender == minted[id - 1].owner, "Operation Allowed!");
         minted[id - 1].cost = newPrice;
         return true;
     }
+
+
 
     function payTo(address to, uint256 amount) internal {
         (bool success, ) = payable(to).call{value: amount}("");
--- scontracts/contracts/ServerContract.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
-import "./ServerContract.sol";
-import "./Brand.sol";
-contract ServerContract {
-  constructor() {
-  }
-}
